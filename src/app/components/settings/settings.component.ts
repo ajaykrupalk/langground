f5394f9e03d4fe7a3a5a75d3e93d0211e0f08446
ajaykrupalk <ajaykrupalk@outlook.in>
2024-06-10 19:20:04 +0530

    changed settings range

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -21,13 +21,13 @@ export class SettingsComponent {
       { value: 'Frequency Penalty', max: 1, min: 0, variable: this.frequencyPenalty, step: 0.1, description: 'Penalizes repeated tokens according to frequency.' }
     ],
     'google': [
-      { value: 'Temperature', max: 2.0, min: 0.0, variable: this.temperature, step: 0.1, description: 'Sampling temperature to use' },
+      { value: 'Temperature', max: 1.0, min: 0.0, variable: this.temperature, step: 0.1, description: 'Sampling temperature to use' },
       { value: 'Max Tokens', max: 4096, min: 0,  variable: this.maxTokens, step: 1, description: 'Maximum number of tokens to generate in the completion.' },
       { value: 'Top P', max: 1, min: 0, variable: this.topP, step: 0.1, description: 'Total probability mass of tokens to consider at each step.'  },
       { value: 'Top K', max: 64, min: 0, variable: this.topK, step: 1, description: 'Changes how the model selects tokens for output.' }
     ],
     'anthropic': [
-      { value: 'Temperature', max: 2.0, min: 0.0, variable: this.temperature, step: 0.1, description: 'Sampling temperature to use' },
+      { value: 'Temperature', max: 1.0, min: 0.0, variable: this.temperature, step: 0.1, description: 'Sampling temperature to use' },
       { value: 'Max Tokens', max: 4096, min: 0,  variable: this.maxTokens, step: 1, description: 'Maximum number of tokens to generate in the completion.' },
       { value: 'Top P', max: 1, min: 0, variable: this.topP, step: 0.1, description: 'Total probability mass of tokens to consider at each step.'  },
       { value: 'Top K', max: 64, min: 0, variable: this.topK, step: 1, description: 'Changes how the model selects tokens for output.' }
